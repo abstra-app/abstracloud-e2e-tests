94bdeb4da7e8ca08c03be5a31c4721fd76f05531
NirvaleO <leo_lmr@hotmail.com>
2022-07-05 19:00:13 -0300

    create more unit input tests in order to test page

--- src/main.py
+++ src/main.py
@@ -7,35 +7,83 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestExamples(unittest.TestCase):
-    def expect_text(self, content):
-        elem = self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, 'text')))
-        self.assertIn(content, elem.text)
-        self.next()
+    def check_if_exists(self, selector):
+        try:
+            elem = self.wait.until(
+                EC.presence_of_element_located((By.XPATH, selector)))
+            return elem
+        except:
+            return False
 
-    def expect_closing_text(self, content):
-        elem = self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, 'text')))
-        self.assertIn(content, elem.text)
-        self.driver.close()
+    def expect_text(self, content, next=True):
+        selector = f'//div[contains(@class,"text") and contains(.,"{content}")]'
+        response = self.check_if_exists(selector)
+        if response is False:
+            self.fail(f'Text {content} not found')
 
-    def expect_link(self, content, url):
-        elem = self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, 'link')))
-        self.assertIn(content, elem.text)
-        self.assertIn(url, elem.get_attribute('href'))
-        self.next()
+        if next:
+            self.next()
 
-    def fill_text(self, label, value):
-        elem = self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, 'label')))
-        self.assertIn(label, elem.text)
-        elem = self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, 'input')))
+    def expect_link(self, content, url, next=True):
+        selector = f'//a[contains(@href,"{url}") and contains(.,"{content}")]'
+        elem = self.check_if_exists(selector)
+        if elem is False:
+            self.fail(f'Link {content} not found')
+        if next:
+            self.next()
+
+    def expect_panda_table(self, columns, next=True):
+        for index, column in enumerate(columns):
+            selector = f'//table/thead/tr/th[{index+2}]'
+            elem = self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, selector)))
+            self.assertIn(column, elem.text)
+        if next:
+            self.next()
+
+    def fill_text(self, label, value, next=True, placeholder="Your answer here"):
+        selector = f'//div[contains(@class,"label") and contains(.,"{label}")]'
+        self.check_if_exists(selector)
+        selector = f'//input[contains(@class,"input") and contains(@placeholder,"{placeholder}")]'
+        elem = self.check_if_exists(selector)
         elem.send_keys(value)
-        self.next()
+        if next:
+            self.next()
 
-    def fill_option(self, label, value):
+    def fill_textarea(self, label, value, next=True, placeholder="Your answer here"):
+        selector = f'//div[contains(@class,"label") and contains(.,"{label}")]'
+        self.check_if_exists(selector)
+        selector = f'//textarea[contains(@class,"input") and contains(@placeholder,"{placeholder}")]'
+        elem = self.check_if_exists(selector)
+        elem.send_keys(value)
+        if next:
+            self.next()
+
+    def fill_phone(self, label, value, next=True, placeholder="(000)000-0000"):
+        selector = f'//div[contains(@class,"label") and contains(.,"{label}")]'
+        elem = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, selector)))
+        self.assertIn(label, elem.text)
+        selector = f'//input[contains(@class,"input") and contains(@placeholder,"{placeholder}")]'
+        elem = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, selector)))
+        elem.send_keys(value)
+        if next:
+            self.next()
+
+    def fill_date(self, label, value, next=True):
+        selector = f'//div[contains(@class,"label") and contains(.,"{label}")]'
+        elem = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, selector)))
+        self.assertIn(label, elem.text)
+        selector = f'//input[contains(@class,"input") and contains(@type,"date")]'
+        elem = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, selector)))
+        elem.send_keys(value)
+        if next:
+            self.next()
+
+    def fill_option(self, label, value, next=True):
         elem = self.wait.until(
             EC.element_to_be_clickable((By.CLASS_NAME, 'label')))
         self.assertIn(label, elem.text)
@@ -43,9 +91,10 @@ class TestExamples(unittest.TestCase):
         elem = self.wait.until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         elem.click()
-        self.next()
+        if next:
+            self.next()
 
-    def fill_multiple_options(self, label, values):
+    def fill_multiple_options(self, label, values, next=True):
         elem = self.wait.until(
             EC.element_to_be_clickable((By.CLASS_NAME, 'label')))
         self.assertIn(label, elem.text)
@@ -54,18 +103,21 @@ class TestExamples(unittest.TestCase):
             elem = self.wait.until(
                 EC.element_to_be_clickable((By.XPATH, selector)))
             elem.click()
-        self.next()
+        if next:
+            self.next()
 
-    def fill_dropdown(self, label, value):
+    def fill_dropdown(self, label, value, next=True):
+        selector = f'//div[contains(@class,"label") and contains(.,"{label}")]'
         elem = self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, 'label'))
+            EC.element_to_be_clickable((By.XPATH, selector))
         )
         self.assertIn(label, elem.text)
         selector = f'//li[contains(@class,"vs__dropdown-option") and contains(.,"{value}")]'
         elem = self.wait.until(
             EC.element_to_be_clickable((By.XPATH, selector)))
         elem.click()
-        self.next()
+        if next:
+            self.next()
 
     def next(self):
         elem = self.wait.until(EC.element_to_be_clickable(
@@ -98,7 +150,8 @@ class TestExamples(unittest.TestCase):
         self.expect_text('I can feel our day')
         self.expect_text('Give me a spin')
         self.expect_link('Try Abstra Cloud free now', 'abstracloud.com')
-        self.expect_closing_text('Thank you')
+        self.expect_text('Thank you', False)
+        self.driver.close()
 
     def test_self_checkin(self):
         self.driver = webdriver.Chrome()
@@ -113,7 +166,7 @@ class TestExamples(unittest.TestCase):
         self.fill_text('What is your last name?', 'Bot')
         self.fill_text('What is your middle initial?', '')
         self.fill_text('Ok. What is your email?', 'abstra@bot.com')
-        self.fill_text('What is your date of birth, Abstra', '30/05/2022')
+        self.fill_date('What is your date of birth, Abstra', '30/05/2022')
         self.fill_dropdown('In which country do you currently live?', 'Brazil')
         self.fill_dropdown(
             'In which city do you currently live?', 'Rio de Janeiro')
@@ -122,27 +175,27 @@ class TestExamples(unittest.TestCase):
         self.fill_text("And what's the number?", 10)
         self.fill_text('Apartment or unit:', 'teste 21231')
         self.fill_text('Zip code:', '123123121231')
-        self.fill_text('What is your primary phone number?', '11999999999')
+        self.fill_phone('What is your primary phone number?', '11999999999')
         self.fill_option(
             'What type of identification can you provide?', "driver's license")
         self.fill_text('What is the identification number?', '123121231')
-        self.fill_text(
+        self.fill_date(
             'What is the identification expiration date?', '30/05/2022')
         self.expect_text(
             "We're done with personal info! Let's move on to your medical history.")
         self.fill_text('What is your last known weight, in kilograms?', '70')
         self.fill_text('What is your height, in centimeters?', '180')
         self.fill_option("Are you under a physician's care now?", 'yes')
-        self.fill_text(
+        self.fill_textarea(
             'Please state the main reason you are under medical care at the moment.', 'aaaaaaaaaaaa')
         self.fill_option(
             'Have you ever been hospitalized or had a major injury?', 'yes')
-        self.fill_text('Please provide details.', 'aaaaaaaaaaaa')
+        self.fill_textarea('Please provide details.', 'aaaaaaaaaaaa')
         self.fill_option('Are you taking any medication?', 'yes')
-        self.fill_text(
+        self.fill_textarea(
             'Please provide reasons, names and dosages of all medication.', 'aaaaaaaaaaaa bbbbbbbb 2mg')
         self.fill_option('Are you on a special diet?', 'yes')
-        self.fill_text(
+        self.fill_textarea(
             'Please provide details regarding your diet.', 'aaaaaaaaaaaa')
         self.fill_option('Do you smoke?', 'yes')
         self.fill_option('Do you consume alcohol regularly?', 'yes')
@@ -150,13 +203,14 @@ class TestExamples(unittest.TestCase):
         self.fill_multiple_options('Please select from the list below any allergies you have.', [
                                    "aspirin", "penicillin", "codein", "local anesthetics", "other"])
         self.fill_text('What other allergies do you have?', 'bbbbbbbb 3mg')
-        self.fill_text(
+        self.fill_textarea(
             'If you have any specific concerns that brought you to medical care today, please explain them in detail here.', 'bbbbbb cccccccc 4mg')
         self.expect_text(
             "Alright! We're nearly finished. Just one last thing...")
         self.fill_text("Please confirm that you've answered this form with the truth to the best of your knowledge by typing your full name EXACTLY as follows: Abstra Bot", 'Abstra Bot')
-        self.expect_closing_text(
-            "Thanks, Abstra Bot! You're checked in and ready to go.")
+        self.expect_text(
+            "Thanks, Abstra Bot! You're checked in and ready to go.", False)
+        self.driver.close()
 
     def test_purchase_requester(self):
         self.driver = webdriver.Chrome()
@@ -177,9 +231,42 @@ class TestExamples(unittest.TestCase):
         self.fill_text('Briefly describe what this expense is for.',
                        'Melhorar a moral da equipe')
         self.fill_option('What type of expense is this?', 'misc')
-        self.fill_text('When is this expense due?', '30/05/2022')
-        self.expect_closing_text(
-            "We've registered this expense succesfully. Thanks! See ya next time.")
+        self.fill_date('When is this expense due?', '30/05/2022')
+        self.expect_text(
+            "We've registered this expense succesfully. Thanks! See ya next time.", False)
+        self.driver.close()
+
+    def test_invoice_factoring_calculator(self):
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
+
+        self.driver.get(
+            "https://examples.abstra.run/56aee472-37a9-49e7-8f4b-9460c84dbc92")
+        self.wait.until(EC.title_is('Invoice Factoring Calculator'))
+        self.next()
+
+        self.expect_text('Hi! Welcome to our Invoice Factoring Calculator.')
+        self.fill_option(
+            'Please choose from the list of example assignors:', "McQueen's Auto Shop")
+        self.expect_text(
+            'Here are the receivables financing terms for this assignor.', False)
+        self.expect_panda_table(
+            ['Assignor Name', 'Monthly Interest Rate'])
+        self.expect_text(
+            "Now, let's calculate terms for a new invoice.", False)
+        self.fill_text(
+            'What is the total value of this invoice?', '500', False)
+        self.fill_date('When is this invoice due?', '30/05/2022')
+        self.fill_dropdown(
+            'Choose a supplier from this list to calculate risk multiplier.', 'Mediocre Thing Doer')
+        self.expect_text(
+            "OK! Mediocre Thing Doer has a risk multiplier of 2 because they sometimes delay payment on invoices but have never defaulted. The new updated interest rate is 2.0%.")
+        self.expect_text(
+            'The take rate for this invoice is calculated at 2.0% over -1 months, given the invoice due date, assignor credit score and relevant supplier historical data.', False)
+        self.expect_text(
+            'The amount payable for this invoice is $510.0.', False)
+
+        self.driver.close()
 
 
 if __name__ == '__main__':
